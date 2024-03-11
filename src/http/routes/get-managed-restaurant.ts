bd5d28b159de1b1622a727526210d02fb322b305
Vicente <vikom.sanchez@gmail.com>
2024-03-11 17:47:43 -0300

    feat(http): add route to get managed restaurant

---
+++ src/http/routes/get-managed-restaurant.ts
@@ -0,0 +1,23 @@
+import { Elysia } from 'elysia'
+import { auth } from '../auth'
+import { db } from '../../db/connection'
+
+export const getManagedRestaurant = new Elysia().use(auth).get(
+  '/managed-restaurant',
+  async ({ getCurrentUser }) => {
+    const { restaurantId } = await getCurrentUser()
+
+    if (!restaurantId) throw new Error('User is not a restaurant manager.')
+
+    const managedRestaurant = await db.query.restaurants.findFirst({
+      where(fields, operators) {
+        return operators.eq(fields.id, restaurantId)
+      },
+    })
+
+    if (!managedRestaurant) throw new Error('Restaurant not found.')
+
+    return managedRestaurant
+  },
+  {},
+)
