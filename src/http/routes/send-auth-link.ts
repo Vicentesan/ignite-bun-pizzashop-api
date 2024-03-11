0f4145617c367c9feaf02ede9436c629c596aee6
Vicente <vikom.sanchez@gmail.com>
2024-03-11 18:14:06 -0300

    feat(mail): add send authenticate email

--- src/http/routes/send-auth-link.ts
+++ src/http/routes/send-auth-link.ts
@@ -3,6 +3,9 @@ import { db } from '../../db/connection'
 import { createId } from '@paralleldrive/cuid2'
 import { authLinks } from '../../db/schema'
 import { env } from '../../env'
+import { mailer } from '../../lib/mailer'
+
+import nodemailer from 'nodemailer'
 
 export const sendAuthLink = new Elysia().post(
   '/authenticate',
@@ -24,14 +27,23 @@ export const sendAuthLink = new Elysia().post(
       userId: userFormEmail.id,
     })
 
-    // TODO: Send email with authLinkCode
-
     const authLink = new URL('/auth-links/authenticate', env.API_BASE_URL)
 
     authLink.searchParams.set('code', authLinkCode)
     authLink.searchParams.set('redirect', env.AUTH_REDIRECT_URL)
 
-    console.log(authLink.toString())
+    const info = await mailer.sendMail({
+      from: {
+        name: 'PizzaShop Inc.',
+        address: 'hi@pizzashop.com',
+      },
+      to: email,
+      subject: 'Authenticate your account to Pizza Shop',
+      html: `<span>Use the following link to authenticate your account on Pizza Shop: <a href="${authLink.toString()}">Authenticate</a><span/>`,
+      text: `Use the following link to authenticate your account on Pizza Shop: ${authLink.toString()}`,
+    })
+
+    console.log(nodemailer.getTestMessageUrl(info))
   },
   {
     body: t.Object({
